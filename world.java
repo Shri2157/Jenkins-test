6f88fc6c4cd0f5dfe9220d7bd9690f32781ca664
Shri <patilshriram982@gmail.com>
2024-07-31 13:38:32 +0530

    pipeline

--- world.java
+++ world.java
@@ -1,5 +1,5 @@
 public class world {
     public static void main(String[] args) {
-        System.out.println("Succesfully runed jenkins pipeline");
+        System.out.println("Succesfully runned test jenkins pipeline After this test2 will execute");
     }
 }
